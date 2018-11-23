476137ada2662795c4bd0e18fef152f88707dd72
FernandoBarrosFCM <fernandobarros@fcmsistemas.com.br>
2018-11-23 12:51:13 -0300

    Commit ajustes

--- interactive_dictonary/app1.py
+++ interactive_dictonary/app1.py
@@ -7,10 +7,26 @@ def translate(word):
   word = word.lower()
   if word in data:
     return data[word]
+  elif word.title() in data:
+    return data[word.title()]
   elif len(get_close_matches(word, data.keys())) > 0:
-    return "Did you mean %s?" % get_close_matches(word, data.keys())[0]
+    yn = input("Did you mean %s? Enter Y if yes or N if no: " % get_close_matches(word, data.keys())[0])
+    if yn == "Y":      
+      return  data[get_close_matches(word, data.keys())[0]]
+    elif yn == "N":
+      return "Word doesn't exist, please check again"
+    else:
+      return "Sorry I don't understand"  
   else:  
     return "Word doesn't exist, please check again"
 
 wordInput = input("Enter a desire word: ")
+
+output = translate(wordInput)
+
+if type(output) == list:  
+  for item in output:  
+    print(item)
+else: 
+  print(output)  
 
